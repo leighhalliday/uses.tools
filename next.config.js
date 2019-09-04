c80eca54145958a1145d7e8b77af3c52c093c609
Leigh Halliday <leighhalliday@gmail.com>
2019-09-04 15:48:28 -0400

    Adding webpack to attemp to fix knex build errors

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
 const withCSS = require("@zeit/next-css");
+const webpack = require("webpack");
 
 module.exports = withCSS({
   webpack(config, _options) {
-    config.externals = config.externals || [];
-    // issues with knex wanting to include mssql
-    // https://github.com/tgriesser/knex/issues/3130
-    config.externals.push({ knex: "commonjs knex" });
+    config.plugins = config.plugins || [];
+    config.plugins = [
+      ...config.plugins,
+      new webpack.IgnorePlugin(/mariasql/, /\/knex\//),
+      new webpack.IgnorePlugin(/mssql/, /\/knex\//),
+      new webpack.IgnorePlugin(/mysql/, /\/knex\//),
+      new webpack.IgnorePlugin(/mysql2/, /\/knex\//),
+      new webpack.IgnorePlugin(/oracle/, /\/knex\//),
+      new webpack.IgnorePlugin(/oracledb/, /\/knex\//),
+      new webpack.IgnorePlugin(/pg-query-stream/, /\/knex\//),
+      new webpack.IgnorePlugin(/sqlite3/, /\/knex\//),
+      new webpack.IgnorePlugin(/strong-oracle/, /\/knex\//),
+      new webpack.IgnorePlugin(/pg-native/, /\/pg\//)
+    ];
 
     return config;
   },
