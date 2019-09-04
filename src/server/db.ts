cef16f9f8b464a3fe844a7248b3412c5c0d09a91
Leigh Halliday <leighhalliday@gmail.com>
2019-09-04 15:25:18 -0400

    Removing alias

--- src/server/db.ts
+++ src/server/db.ts
@@ -3,11 +3,8 @@ import knex from "knex";
 const db = knex({
   client: "pg",
   connection: process.env.PG_CONNECTION_STRING,
-  debug: true,
-  acquireConnectionTimeout: 5000
+  debug: false
 });
-
-console.log("PG_CONNECTION_STRING", process.env.PG_CONNECTION_STRING);
 
 export async function findToolBy(
   field: string,
